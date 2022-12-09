5b206df475e41d506afc1e03e933a914528dcd3b
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-12-09 21:36:05 +0600

    ch4 page 48

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,55 @@
-// index.js
-// This is the main entry point of our application
-
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hellow World !!!');
-});
+let notes = [
+  {
+    id: '1',
+    content: 'This is a note',
+    author: 'Adam Scott'
+  },
+  {
+    id: '2',
+    content: 'This is another note',
+    author: 'Harlow Everly'
+  },
+  {
+    id: '3',
+    content: 'Oh hey look, another note!',
+    author: 'Riley Harrison'
+  }
+];
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Построение схемы Graph QL
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String
+    notes: [Note!]!
+  }
+`;
+
+//  Предоставим функцию распознаватель для полей схемы
+const resolvers = {
+  Query: {
+    hello: () => 'Hellow World!',
+    notes: () => notes
+  }
+};
+
+// Настройка Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Применяем промежуточное ПО Apollo Graph QL и указываем путь к /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () => {
+  console.log(
+    `Graph QL server running at http://localhost:${port}${server.graphqlPath}`
+  );
 });
