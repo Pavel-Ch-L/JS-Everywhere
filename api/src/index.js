8b2ae74c23789a62d0601f87de76d4d4a794b5bf
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-12-14 20:54:55 +0600

    ch5 next read and write data page 59

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
+const db = require('./db');
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+console.log(process.env.DB_HOST);
+// Подключаем БД
+db.connect(DB_HOST);
 
 let notes = [
   {
