245ff5312970817e87549c10381593f64d4d7d05
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-12-19 20:44:10 +0600

    ch8 next favorites notes page 95

--- api/src/resolvers/query.js
+++ api/src/resolvers/query.js
@@ -5,5 +5,17 @@ module.exports = {
   },
   note: async (parent, args, { models }) => {
     return await models.Note.findById(args.id);
+  },
+  user: async (parent, { username }, { models }) => {
+    // Находим пользователя по имени
+    return await models.User.findOne({ username });
+  },
+  users: async (parent, args, { models }) => {
+    // Находим всех пользователей
+    return await models.User.find({});
+  },
+  me: async (parent, args, { models, user }) => {
+    // Находим пользователя по текущему пользовательскому контенту
+    return await models.User.findById(user.id);
   }
 };
--- api/src/schema.js
+++ api/src/schema.js
@@ -22,6 +22,9 @@ module.exports = gql`
     hello: String
     notes: [Note!]!
     note(id: ID!): Note!
+    user(username: String!): User!
+    users: [User!]!
+    me: User!
   }
   type Mutation {
     newNote(content: String!): Note!
