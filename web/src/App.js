1f81660eb44b2db14c4b7497c390530fc3b07f26
Pavel_Ch__(L) <pavel79.su@gmail.com>
2023-01-05 18:01:04 +0600

    ch14 page 154 query to API

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// Импортируем библиотеки Apollo Client
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 // Импортируем глобальные стили
 import GlobalStyle from './components/GlobalStyle';
 // Импортируем маршруты
 import Pages from './pages';
+// Настраиваем API URI и кэш
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+// Настраиваем Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
 
-const App = () => {
-  return (
-    <div>
-      <GlobalStyle />
-      <Pages />
-    </div>
-  );
-};
+const App = () => (
+  <ApolloProvider client={client}>
+    <GlobalStyle />
+    <Pages />
+  </ApolloProvider>
+);
 
 ReactDom.render(<App />, document.getElementById('root'));
