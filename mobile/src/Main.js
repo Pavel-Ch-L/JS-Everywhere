4da2c458159fc85c6d14b6c3718b6cf2ec678047
Pavel_Ch__(L) <pavel79.su@gmail.com>
2023-03-11 14:21:52 +0600

    ch22 next Style

--- mobile/src/Main.js
+++ mobile/src/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 const Main = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hello world!</Text>
+      <Image source={require('../assets/images/hello-world.jpg')} />
     </View>
   );
 };
